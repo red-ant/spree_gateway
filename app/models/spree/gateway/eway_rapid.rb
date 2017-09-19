a0a3c2cc80587e5ad9a6f599924c6952c0772166
Dan Callaghan <dancallaghan@redant.com.au>
2017-09-19 15:49:42 +1000

    eWAY Rapid: add client side encryption

--- app/models/spree/gateway/eway_rapid.rb
+++ app/models/spree/gateway/eway_rapid.rb
@@ -2,6 +2,11 @@ module Spree
   class Gateway::EwayRapid < Gateway
     preference :login, :string
     preference :password, :string
+    preference :client_side_encryption_key, :text
+
+    def method_type
+      'eway_rapid'
+    end
 
     def provider_class
       ActiveMerchant::Billing::EwayRapidGateway
